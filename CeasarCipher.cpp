d6ecb13601d7dfc275bbd2724f209e6acce3809f
Binayak Niraula <binayak0128@gmail.com>
2024-06-25 14:31:47 +0545

    Added Caesar encrypt and Rail Fence

---
+++ CeasarCipher.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void encrypt(char text[], int shift){
+	for(int i=0; i<strlen(text);i++){
+		if(text[i]>='a'&& text[i]<='z'){
+			text[i]='a'+(text[i]-'a'+shift)%26;
+		}
+		else if{
+				text[i]='A'+(text[i]-'A'+shift)%26;
+		}
+	}
+}
+int main(){
+	char plaintext[100];
+	int shift;
+	printf("Enter plain text:\n");
+	scanf("%[^\n]s",&plaintext);
+	printf("Enter shift value: \n");
+	scanf("%d",&shift);
+	encrypt(plaintext,shift);
+	printf("Encrypted text :- \n"plaintext);
+	return 0;
+}
